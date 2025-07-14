c2d796d96a82eb10df3ab823127104f8db0ca045
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-14 14:10:54 +0200

    initial commit:

---
+++ u2-w1-d1-spingIntro/src/main/java/andreamaiolo/u2_w1_d1_spingIntro/U2W1D1SpingIntroApplication.java
@@ -0,0 +1,13 @@
+package andreamaiolo.u2_w1_d1_spingIntro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D1SpingIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D1SpingIntroApplication.class, args);
+	}
+
+}
---
+++ u2-w1-d1-spingIntro/src/test/java/andreamaiolo/u2_w1_d1_spingIntro/U2W1D1SpingIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package andreamaiolo.u2_w1_d1_spingIntro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D1SpingIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
